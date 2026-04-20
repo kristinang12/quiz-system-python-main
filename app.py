c13ed1a7777771143948ddd6ebced0b326a22b59
kristinang12 <kristinangalcober12@gmail.com>
2026-04-20 14:20:49 +0800

    edit app

--- app.py
+++ app.py
@@ -101,10 +101,14 @@ def clear_scores():
 
 # ── Start the server ──────────────────────────────────────────
 
+#if __name__ == "__main__":
+    #print("=" * 50)
+    #print("  Accessible Gamified Quiz System")
+    #print("  Running at: http://127.0.0.1:5000")
+    #print("  Press CTRL+C to stop the server")
+    #print("=" * 50)
+    #app.run(debug=True)
+    
 if __name__ == "__main__":
-    print("=" * 50)
-    print("  Accessible Gamified Quiz System")
-    print("  Running at: http://127.0.0.1:5000")
-    print("  Press CTRL+C to stop the server")
-    print("=" * 50)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
